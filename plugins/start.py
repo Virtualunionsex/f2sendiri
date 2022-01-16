fefcaa71f9335dba9478fcc7a4b6f38324f1cc91
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2022-01-16 19:01:18 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -146,7 +146,7 @@ async def not_joined(client: Client, message: Message):
             InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink),
             InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink2),
          
-        [   InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink),
+            InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink),
             InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink2),
         ],  
     ]
