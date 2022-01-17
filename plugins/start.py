c0800c5a588e3cc052a721ba9fd77c639733da0a
jokokendi <80366713+jokokendi@users.noreply.github.com>
2022-01-17 10:07:23 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -116,6 +116,10 @@ async def start_command(client: Client, message: Message):
             [
                 InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink),
                 InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton("♻️𝗖𝗛𝗔𝗡𝗡𝗘𝗟♻️", url=client.invitelink3),
+                InlineKeyboardButton("♻️𝗚𝗥𝗢𝗨𝗣♻️", url=client.invitelink4), 
             ],  
             [
                 InlineKeyboardButton("🔅 ᴛᴜᴛᴜᴘ 🔅", callback_data="close"),
