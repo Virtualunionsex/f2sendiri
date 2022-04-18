37a6a077a994308dba77e98afc120fc82456046f
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-18 09:02:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
-# (©)Codexbotz
-# Recode By @Mafia_Tobatz
-# Kalo clone Gak usah hapus 
-# gue tandain akun tele nya ngentod
+
 
 import pyromod.listen
 import sys
@@ -15,7 +12,6 @@ from config import (
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
     FORCE_SUB_CHANNEL1,
-    FORCE_SUB_GROUP1,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -52,7 +48,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -68,7 +64,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL1:
@@ -84,23 +80,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
-                )
-                sys.exit()
-        if FORCE_SUB_GROUP1:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_GROUP1)
-                self.invitelink4 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_GROUP1!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_GROUP1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
                 )
                 sys.exit()
         try:
