fb2a5f4975e71f0437d8c0276a3bc30d94cd4b31
Virtualunionsex <97935254+Virtualunionsex@users.noreply.github.com>
2022-06-05 00:29:43 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -116,15 +116,15 @@ async def start_command(client: Client, message: Message):
             [InlineKeyboardButton("• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about")],
             [
                 InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink),
-                InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink2),
+                InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink2),
             ],
             [
                 InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink3),
                 InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink4),
             ],  
             [
-                InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink5),
-                InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink6),
+                InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink5),
+                InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink6),
             ],  
             [
                 InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close"),
@@ -155,15 +155,15 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink), 
-            InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink2),
+            InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink2),
         ],
         [
             InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink3), 
              InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink4),
             ],  
             [
-                InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink5),
-                InlineKeyboardButton("🌐𝗖𝗛𝗔𝗡𝗡𝗘𝗟🌐", url=client.invitelink6),
+                InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink5),
+                InlineKeyboardButton("🌐𝗚𝗥𝗢𝗨𝗣🌐", url=client.invitelink6),
         ],
     ]
     try:
