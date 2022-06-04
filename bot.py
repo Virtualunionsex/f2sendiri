eba547fdbc6ec26d0a966e943059d2d656ab0169
Virtualunionsex <97935254+Virtualunionsex@users.noreply.github.com>
2022-06-05 00:37:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,9 +67,7 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+               
                 )
                 sys.exit()
          if FORCE_SUB_CHANNEL2:
@@ -83,9 +81,7 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+                
                 )
                 sys.exit() 
          if FORCE_SUB_CHANNEL3:
@@ -99,9 +95,7 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+               
                 )
                 sys.exit()        
         if FORCE_SUB_GROUP:
@@ -131,9 +125,7 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+               
                 )
                 sys.exit()
         try:
