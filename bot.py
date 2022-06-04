51b0c18deede202660694191d298970a62d0a7fc
Virtualunionsex <97935254+Virtualunionsex@users.noreply.github.com>
2022-06-04 23:42:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,11 @@ from config import (
     APP_ID,
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
-    FORCE_SUB_GROUP,
     FORCE_SUB_CHANNEL1,
+    FORCE_SUB_CHANNEL2,
+    FORCE_SUB_CHANNEL3,
+    FORCE_SUB_GROUP,
+    FORCE_SUB_GROUP1,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -53,6 +56,54 @@ class Bot(Client):
                     "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
                 )
                 sys.exit()
+         if FORCE_SUB_CHANNEL1:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
+                self.invitelink3 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+                )
+                sys.exit()
+         if FORCE_SUB_CHANNEL2:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
+                self.invitelink3 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+                )
+                sys.exit() 
+         if FORCE_SUB_CHANNEL3:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL3)
+                self.invitelink3 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
+                )
+                sys.exit()        
         if FORCE_SUB_GROUP:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_GROUP)
@@ -69,9 +120,9 @@ class Bot(Client):
                     "\nBot Berhenti. Gabung Group https://t.me/ruangdiskusikami untuk Bantuan"
                 )
                 sys.exit()
-        if FORCE_SUB_CHANNEL1:
+        if  FORCE_SUB_GROUP1:
             try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
+                link = await self.export_chat_invite_link( FORCE_SUB_GROUP1)
                 self.invitelink3 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
@@ -102,7 +153,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/Punya_Alby"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/Panggilsaja_m"
         )
         self.username = usr_bot_me.username
 
